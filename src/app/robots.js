495216a22c98accc97e31893a4726c5a376739d5
dikiwahyudin <74003372+dixs-bot@users.noreply.github.com>
2026-09-02 15:13:39 +0700

    Add files via upload

---
+++ src/app/robots.js
@@ -0,0 +1,22 @@
+import { siteConfig } from "../data/siteConfig";
+
+export default function robots() {
+  const baseUrl = siteConfig.siteUrl.replace(/\/$/, "");
+
+  return {
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: [
+          "/api/",
+          "/_next/",
+        ],
+      },
+    ],
+
+    sitemap: `${baseUrl}/sitemap.xml`,
+
+    host: baseUrl,
+  };
+}
